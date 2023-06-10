38455702762ed87397a6829427000eb6667eacec
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-06-10 14:16:30 -0300

    feat: add menu navegação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './styles/estilosGlobais.scss';
 
 import PaginaInicial from "./paginas/pagina-inicial";
+import Menu from './componentes/menu';
 
 function App() {
   return (
-    <PaginaInicial />
+    <>
+      <PaginaInicial />
+      <Menu />
+    </>
   )
 }
 
