25ab83cb1f4194904af8904653af5411f2822605
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-06-12 13:26:28 -0300

    config: add absolute imports

--- src/paginas/pagina-inicial/index.jsx
+++ src/paginas/pagina-inicial/index.jsx
@@ -1,10 +1,11 @@
-import Cabecalho from "../../componentes/cabecalho";
-import Menu from "../../componentes/menu";
+import Cabecalho from "componentes/cabecalho";
+import Menu from "componentes/menu";
+import Banner from "componentes/banner";
+import Rodape from "componentes/rodape";
+import Galeria from "componentes/galeria";
+import Populares from "componentes/populares";
+
 import styles from "./PaginaInicial.module.scss";
-import Banner from "../../componentes/banner";
-import Rodape from "../../componentes/rodape";
-import Galeria from "../../componentes/galeria";
-import Populares from "../../componentes/populares";
 
 const PaginaInicial = () => {
     return(
