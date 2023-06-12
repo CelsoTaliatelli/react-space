bb01b3910d735b5a10b082d00e3a2a84384938a6
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-06-12 13:50:45 -0300

    add funcionalidade de filtros por tag

--- src/componentes/galeria/index.jsx
+++ src/componentes/galeria/index.jsx
@@ -1,14 +1,24 @@
 import Tags from "../tags";
 import styles from "./Galeria.module.scss";
-import fotos from "./fotos.json";
+import fotosJson from "./fotos.json";
 
 import Cards from "./cards";
+import { useState } from "react";
 
 const Galeria = () => {
+    const [fotos, setFotos] = useState(fotosJson);
+    const tags = [...new Set(fotos.map((foto) => foto.tag))];
+    const filtraFotos = (tag) => {
+        const novasFotos = fotos.filter((foto) => {
+            return foto.tag === tag;
+        })
+       setFotos(novasFotos); 
+    }
+
     return(
         <section className={styles.galeria}>
             <h2>Navegue pela galeria</h2>
-            <Tags />
+            <Tags tags={tags} filtrarFotos={filtraFotos} setFotos={setFotos}/>
             <Cards fotos={fotos} styles={styles}/>
         </section>
     );
--- src/componentes/tags/index.jsx
+++ src/componentes/tags/index.jsx
@@ -1,14 +1,17 @@
 import styles from "./Tags.module.scss";
+import fotos from "../galeria/fotos.json"
 
-const Tags = () => {
+const Tags = ({tags, filtrarFotos, setFotos}) => {
     return(
         <div className={styles.tags}>
             <p>Filtre por tags:</p>
             <ul className={styles.tags__lista}>
-                <li>Estrelas</li>
-                <li>Galáxias</li>
-                <li>Lua</li>
-                <li>Planetas</li>
+                {tags.map((tag) => {
+                    return(
+                        <li key={tag} onClick={() => filtrarFotos(tag)}>{tag}</li>
+                    );
+                })}
+                <li onClick={() => setFotos(fotos)}>Todas</li>
             </ul>
         </div>
     );
